249dd2e618c26b0cf2948e60079c5884c73e3911
tomatojojo <joshejercito01@gmail.com>
2022-11-26 21:57:17 +0800

    add: messaging back and forth

--- Client.py
+++ Client.py
@@ -1,17 +1,44 @@
 import socket
+import threading
+import random
 
-msgFromClient       = "Hello UDP Server"
-bytesToSend         = str.encode(msgFromClient)
-serverAddressPort   = ("127.0.0.1", 3000)
-bufferSize          = 4096
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client.bind(("localhost", random.randint(8000,9000)))
+
+name = input("Nickname: ")
+
+def receive():
+    while True:
+        try:
+            message, _ = client.recvfrom(2048)
+            print(message.decode())
+        except:
+            pass
+
+t = threading.Thread(target=receive)
+t.start()
+
+client.sendto(f"Online Tag:{name}".encode(), ("localhost", 3000))
+
+while True:
+    message = input("")
+    if message == "!q":
+        exit()
+    else:
+        client.sendto(f"{name}: {message}".encode(), ("localhost", 3000))
+
+# msgFromClient       = "Hello UDP Server"
+# bytesToSend         = str.encode(msgFromClient)
+# serverAddressPort   = ("127.0.0.1", 3000)
+# bufferSize          = 4096
 
 
-UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+# UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+# UDPClientSocket.sendto(bytesToSend, serverAddressPort)
 
-msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+# msgFromServer = UDPClientSocket.recvfrom(bufferSize)
 
 
-msg = "Message from Server {}".format(msgFromServer[0])
+# msg = "Message from Server {}".format(msgFromServer[0])
 
-print(msg)
+# print(msg)
--- Server.py
+++ Server.py
@@ -1,25 +1,64 @@
 import socket
+import threading
+import queue
 
-localIP = "127.0.0.1"
-localPort = 3000
-bufferSize = 4096
-serverMessage = "Hello World"
-bytesCount = str.encode(serverMessage)
+messages = queue.Queue()
+clients = []
 
-socketUDP = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-socketUDP.bind((localIP, localPort))
+# localIP = "127.0.0.1"
+# localPort = 3000
+# bufferSize = 4096
+# serverMessage = "Hello World"
+# bytesCount = str.encode(serverMessage)
 
-print("UDP Server is online")
+# socketUDP = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+# socketUDP.bind((localIP, localPort))
 
-while(True):
-    bytesAddress = socketUDP.recvfrom(bufferSize)
-    message = bytesAddress[0]
-    address = bytesAddress[1]
+# print("UDP Server is online")
 
-    clientMsg = "Message from Client:{}".format(message)
-    clientIP ="Client IP Address;{}".format(address)
+# while(True):
+#     bytesAddress = socketUDP.recvfrom(bufferSize)
+#     message = bytesAddress[0]
+#     address = bytesAddress[1]
 
-    print(clientMsg)
-    print(clientIP)
+#     clientMsg = "Message from Client: {}".format(message)
+#     clientIP ="Client IP Address: {}".format(address)
 
-    socketUDP.sendto(bytesCount, address)
+#     print(clientMsg)
+#     print(clientIP)
+
+#     socketUDP.sendto(bytesCount, address)
+
+server = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+server.bind(("localhost", 3000))
+
+def receive():
+    while True:
+        try:
+            message, address= server.recvfrom(2048)
+            messages.put((message, address))
+        except:
+            pass
+
+def broadcast():
+    while True:
+        while not messages.empty():
+            message, address = messages.get()
+            print(message.decode())
+            if address not in clients:
+                clients.append(address)
+            for client in clients:
+                try:
+                    if message.decode().startswith("Online Tag:"):
+                        name = message.decode()[message.decode().index(":")+1:]
+                        server.sendto(f"{name} joined!".encode(), client)
+                    else:
+                        server.sendto(message, client)
+                except:
+                    clients.remove(client)
+
+t1 = threading.Thread(target=receive)
+t2 = threading.Thread(target=broadcast)
+
+t1.start()
+t2.start()
