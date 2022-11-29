b1ade09a60d916d25c67cf2748317e42758facef
Anton1017 <anton_morana@dlsu.edu.ph>
2022-11-30 00:26:13 +0800

    anton update

--- Client.py
+++ Client.py
@@ -2,6 +2,46 @@ import socket
 import threading
 import random
 
+joined = false;
+
+
+while not joined:
+    command = input()
+    command.split()
+    if command[0] == "/join":
+        if command[1] == "127.0.0.1":
+            if command[2] =="3000":
+                joined = True
+            else:
+                print("Error: Connection to the Message Board Server has failed! Please check IP Address and Port Number.")
+        else:
+            print("Error: Connection to the Message Board Server has failed! Please check IP Address and Port Number.")
+    elif command[0] == "/leave":
+        print("Error: Disconnection failed. Please connect to the server first.")
+    else:
+        print("Error: Command not found.")
+
+
+
+
+
+msgFromClient       = "Hello UDP Server"
+bytesToSend         = str.encode(msgFromClient)
+serverAddressPort   = ("127.0.0.1", 3000)
+bufferSize          = 4096
+
+
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+
+msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+
+
+msg = "Message from Server {}".format(msgFromServer[0])
+
+print(msg)
+
+'''
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client.bind(("localhost", random.randint(8000,9000)))
 
@@ -28,19 +68,4 @@ while True:
         
     else:
         client.sendto(f"{name}: {message}".encode(), ("localhost", 3000))
-
-# msgFromClient       = "Hello UDP Server"
-# bytesToSend         = str.encode(msgFromClient)
-# serverAddressPort   = ("127.0.0.1", 3000)
-# bufferSize          = 4096
-
-
-# UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-# UDPClientSocket.sendto(bytesToSend, serverAddressPort)
-
-# msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-
-
-# msg = "Message from Server {}".format(msgFromServer[0])
-
-# print(msg)
+'''
--- Server.py
+++ Server.py
@@ -2,32 +2,35 @@ import socket
 import threading
 import queue
 
+
+localIP = "127.0.0.1"
+localPort = 3000
+bufferSize = 4096
+serverMessage = "Hello World"
+bytesCount = str.encode(serverMessage)
+
+socketUDP = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+socketUDP.bind((localIP, localPort))
+
+print("UDP Server is online")
+
+while(True):
+    bytesAddress = socketUDP.recvfrom(bufferSize)
+    message = bytesAddress[0]
+    address = bytesAddress[1]
+
+    clientMsg = "Message from Client: {}".format(message)
+    clientIP ="Client IP Address: {}".format(address)
+
+    print(clientMsg)
+    print(clientIP)
+
+    socketUDP.sendto(bytesCount, address)
+
+
+'''
 messages = queue.Queue()
 clients = []
-
-# localIP = "127.0.0.1"
-# localPort = 3000
-# bufferSize = 4096
-# serverMessage = "Hello World"
-# bytesCount = str.encode(serverMessage)
-
-# socketUDP = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-# socketUDP.bind((localIP, localPort))
-
-# print("UDP Server is online")
-
-# while(True):
-#     bytesAddress = socketUDP.recvfrom(bufferSize)
-#     message = bytesAddress[0]
-#     address = bytesAddress[1]
-
-#     clientMsg = "Message from Client: {}".format(message)
-#     clientIP ="Client IP Address: {}".format(address)
-
-#     print(clientMsg)
-#     print(clientIP)
-
-#     socketUDP.sendto(bytesCount, address)
 
 server = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 server.bind(("localhost", 3000))
@@ -62,3 +65,4 @@ t2 = threading.Thread(target=broadcast)
 
 t1.start()
 t2.start()
+'''
